a88d7d7b14739cc5782b751d51d18ebec0bd7081
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-08 15:32:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,8 +60,8 @@ function dec(n){
 function makeInt(n){
     return parseInt(n)
 }
-makeInt(parseInt(n,10))
-// function makeInt(n){
-//     return parseInt(n,10)
-// }
+
+function makeInt(n){
+    return parseInt(n,10)
+}
 
